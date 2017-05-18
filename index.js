9008c09323668b8f4a1f9980c771928bca4ddb01
sur1274369 <sur1274369@gmail.com>
2017-05-18 19:15:03 +0400

    modifaed index

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 "use strict";
 
-function speak(){
-  console.log('hello')
+const speak = function(){
+  const arr = ['foo','bar','baz'];
+  for(const i of arr){
+    console.log(i.toUpperCase());
+  }
 };
 speak();
