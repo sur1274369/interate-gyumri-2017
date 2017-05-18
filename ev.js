c1f50dd65b8fc09a73e56e5557152e27e67d517f
sur1274369 <sur1274369@gmail.com>
2017-05-18 20:53:49 +0400

    ev changsss

--- ev.js
+++ ev.js
@@ -1,9 +1,15 @@
 'use strict';
 
-let counter = 0;
- const interval = setInterval(function(){
-   console.log(`Called ${++counter}times`);
-   if(counter == 8){
-     clearInterval(interval);
-   }
- },500)
+// let counter = 0;
+//  const interval = setInterval(function(){
+//    console.log(`Called ${++counter}times`);
+//    if(counter == 8){
+//      clearInterval(interval);
+//    }
+//  },500)
+
+
+process.stdin.on('data',function(d){
+  console.log(d);
+  console.log(d.toString().toUpperCase())
+})
